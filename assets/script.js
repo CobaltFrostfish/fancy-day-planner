484b1a4640dba7cae70287d6deba49ab0b15b3e0
Chris Whalen <christopher.whalen@du.edu>
2021-03-21 12:08:25 -0600

    Save function finally working

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var datetime = null,
 
 var update = function () {
     date = moment(new Date())
-    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+    datetime.html(date.format('dddd, MMMM Do YYYY, HH:mm:ss'));
 };
 
 $(document).ready(function(){
@@ -11,4 +11,23 @@ $(document).ready(function(){
     update();
     setInterval(update, 1000);
 });
+
+
+$('.saveBtn').on('click', function(event){
+    var value = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
+
+    localStorage.setItem(time, value);
+})
+
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
 
