2131dae5387d7c70f1383bca19e346f1b8ec8840
Chris Whalen <christopher.whalen@du.edu>
2021-03-21 12:58:10 -0600

    JavaScript file complete and functioning

--- assets/script.js
+++ assets/script.js
@@ -30,4 +30,22 @@ $('#hour-15 .description').val(localStorage.getItem('hour-15'));
 $('#hour-16 .description').val(localStorage.getItem('hour-16'));
 $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
+function colorUpdate() {
+    var currentHour = moment().hours()
+    console.log(currentHour)
+    $(".time-block").each(function(){
+        var blockNumber = parseInt($(this).attr("id").split("-")[1])
+        if (blockNumber < currentHour) {
+            $(this).addClass("past")
+        }
+        else if (blockNumber === currentHour) {
+            $(this).addClass("present")
+        }
+        else {
+            $(this).addClass("future")
+        }
 
+    })
+}
+
+colorUpdate()
